e7c79fb586e210dc63bd2e574201164ac682ea0a
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-10-04 21:58:42 +0500

    Second Video

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  const userName = "Abdullah Baig";
+  const avatar = "https://avatars.githubusercontent.com/u/112177631?s=96&v=4";
+  const loggedIn = true;
+
+  const student = [
+    { name: "Altaf", key: 1 },
+    { name: "Ali", key: 2 },
+    { name: "Adil", key: 3 },
+  ];
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <h1>Hello {userName}</h1>
+      <img src={avatar} alt="Abdullah Baig" className="photo" />
+      <ul>
+        {student.map((data) => (
+          <li key={data.key}>{data.name}</li>
+        ))}
+      </ul>
+
+      <br />
+      <button> {loggedIn ? "Logout" : "Login"} </button>
+    </>
   );
 }
 
-export default App
+export default App;
